8968ee43ee2a8510e6d7ede37b5297cbccab1128
Martin Bojmaliev <mbojmaliev@gmail.com>
2022-08-12 22:17:44 +0200

    footer changes

--- resources/views/components/layouts/signed/footer.blade.php
+++ resources/views/components/layouts/signed/footer.blade.php
@@ -1,14 +1,13 @@
 @stack('footer_start')
 
-    <footer class="footer">
-        <div class="flex flex-col sm:flex-row items-center justify-center mt-10 lg:mt-20 py-7 text-sm font-light">
-            <div>
-                {{ trans('footer.powered') }}:
-                <a href="{{ trans('footer.link') }}" target="_blank">{{ trans('footer.software') }}</a>
-                &nbsp;<span class="material-icons align-middle text-black-300">code</span>&nbsp;
-                {{ trans('footer.version') }} {{ version('short') }}
-            </div>
+<footer class="footer">
+    <div class="flex flex-col sm:flex-row items-center justify-center mt-10 lg:mt-20 py-7 text-sm font-light">
+        <div>
+            <a href="{{ trans('footer.link') }}" target="_blank">
+                {{ trans('footer.powered') }}
+            </a>
         </div>
-    </footer>
+    </div>
+</footer>
 
 @stack('footer_end')
