527d33a9a022e2aa42905f76f47d431681498f36
Martin Bojmaliev <mbojmaliev@gmail.com>
2022-08-13 02:19:51 +0200

    csrf

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -19,6 +19,6 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        '/*/portal/nlb-bank/invoices/*/callback'
+        '/*/signed/nlb-bank/invoices/*/callback'
     ];
 }
