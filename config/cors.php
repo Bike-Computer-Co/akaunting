503bb856bb18abd06a2784eb70a0f7b278ed45a9
Martin Bojmaliev <mbojmaliev@gmail.com>
2022-08-25 22:41:28 +0200

    localho.st

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*.digitalhub.mk', 'digitalhub.mk', 'localhost:3000'],
+    'allowed_origins' => ['*.digitalhub.mk', 'digitalhub.mk', 'localho.st:3000'],
 
     'allowed_origins_patterns' => [],
 
