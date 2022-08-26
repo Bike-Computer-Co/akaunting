51cfe694cc6ed2934d4eefcab8515bb52e90c4dc
Martin Bojmaliev <mbojmaliev@gmail.com>
2022-08-26 19:34:05 +0200

    Company register

--- app/Http/Controllers/ApiPublic/CompanyRegister.php
+++ app/Http/Controllers/ApiPublic/CompanyRegister.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\ApiPublic;
 
 use App\Abstracts\Http\Controller;
+use App\Abstracts\Notification;
+use App\Notifications\RegisterCompanyRequestNotification;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -10,6 +12,32 @@ class CompanyRegister extends Controller
 {
     public function request(Request $request)
     {
+        $validated = $request->validate([
+            'merchant' => 'required',
+            'company_name' => 'required',
+            'name' => 'required',
+            'address' => 'required',
+            'personal_id' => 'required',
+            'same_director_button' => 'required',
+            'name_director' => 'required',
+            'address_director' => 'required',
+            'personal_id_director' => 'required',
+            'bank' => 'required',
+            'street' => 'required',
+            'number' => 'required',
+            'city' => 'required',
+            'same_address_button' => 'required',
+            'street_partner' => 'required',
+            'number_partner' => 'required',
+            'city_partner' => 'required',
+            'selected_activity' => 'required',
+            'option_1' => 'required',
+            'option_2' => 'required'
+        ]);
+
+        \Illuminate\Support\Facades\Notification::route('mail', 'ivan@venikom.com')
+            ->notify(new RegisterCompanyRequestNotification($validated));
+
         return JsonResource::make($request->all());
     }
 }
---
+++ app/Notifications/RegisterCompanyRequestNotification.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use phpDocumentor\Reflection\Types\This;
+
+class RegisterCompanyRequestNotification extends Notification
+{
+    use Queueable;
+
+    private array $data;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param mixed $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param mixed $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+
+
+        $data = $this->data;
+        return (new MailMessage)
+            ->subject('Барање за регистрирање на фирма')
+            ->greeting('Здраво Иван,')
+            ->line("Имате нова порака од $data[name]")
+            ->line("")
+            ->line("Форма 1: $data[merchant]")
+            ->line("Назив на Друштвото: $data[company_name]")
+            ->line("")
+            ->line("Податоци за основач на друштво:")
+            ->line("Име: $data[name]")
+            ->line("Презиме: $data[address]")
+            ->line("ЕМБГ: $data[personal_id]")
+            ->line("")
+            ->line("Податоци за управител:")
+            ->line("Податоците за управителот се исти со податоците на основачот: $data[same_director_button]")
+            ->line("Име: $data[name_director]")
+            ->line("Презиме: $data[address_director]")
+            ->line("ЕМБГ: $data[personal_id_director]")
+            ->line("Банка: $data[bank]")
+            ->line("")
+            ->line("Седиште на Фирма:")
+            ->line("Адреса:")
+            ->line("улица:  $data[street]")
+            ->line("број:  $data[number]")
+            ->line("град: $data[city]")
+            ->line("")
+            ->line("Адреса на подружница:")
+            ->line("Адресата на подружницата е иста со седиштето на фирмата:  $data[same_address_button]")
+            ->line("")
+            ->line("Адреса:")
+            ->line("улица:  $data[street_partner]")
+            ->line("број:  $data[number_partner]")
+            ->line("град:  $data[city_partner]")
+            ->line("")
+            ->line("Дејност со која ќе се занимава - Шифра: $data[selected_activity]")
+            ->line("")
+            ->line("Основна главина:")
+            ->line("Внесување на главина: $data[option_1]")
+            ->line("Влог: $data[option_2]")
+            ->salutation("Поздрав, $data[name]");
+
+    }
+}
