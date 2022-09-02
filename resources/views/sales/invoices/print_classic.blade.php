e5e13abd982bb3785a448f836765907a53910ff3
Martin Bojmaliev <mbojmaliev@gmail.com>
2022-09-02 15:24:40 +0200

    improvments on qr code

--- resources/views/sales/invoices/print_classic.blade.php
+++ resources/views/sales/invoices/print_classic.blade.php
@@ -11,13 +11,13 @@
         @php
             $shortUrl = $invoice->short_url;
         @endphp
-        <img src="https://api.qrserver.com/v1/create-qr-code/?data={{$shortUrl}}&size=100x100&margin=0" alt="">
+        <img src="https://api.qrserver.com/v1/create-qr-code/?data={{$shortUrl}}&size=80x80&margin=0" alt="">
         <br>
-        <small>
+        <p style="font-size: 12px; color: #424242;">
             Pay online:
             <a style="text-decoration: none; color:black" href="{{$shortUrl}}">
                 {{$shortUrl}}
             </a>
-        </small>
+        </p>
     </x-slot>
 </x-layouts.print>
