57dc0556d512af86b70f065183f612a47aad4928
Jordancbo Eftimov <ejordancho@gmail.com>
2022-11-30 15:17:42 +0100

    Fixed 500 for recurring

--- app/Http/Middleware/CheckBilling.php
+++ app/Http/Middleware/CheckBilling.php
@@ -16,22 +16,6 @@ class CheckBilling
      */
     public function handle(Request $request, Closure $next)
     {
-
-        //invite acountant
-        $check1 = $request->routeIs('users.store') && $request->roles == 4 && !company()->haveOption('invite_accountant');
-        $check2 = $request->routeIs('recurring-invoices.*')  && !company()->haveOption('recurring_invoices');
-        $check3 = $request->routeIs('settings.schedule.update') && !company()->haveOption('remind');
-        if ($check1 || $check2) {
-            flash('Ве молиме надградете го вашиот пакет за да ја користите таа опција')->error()->important();
-            if ($request->expectsJson()) {
-                return response()->json([
-                    'redirect' => route('billing.subscription')
-                ]);
-            } else {
-                return redirect()->route('billing.subscription');
-            }
-        }
-
         return $next($request);
     }
 
