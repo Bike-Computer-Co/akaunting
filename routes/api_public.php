c0d822e3bd5fe15fdf3bf8db63b03b15a886bb59
Jordancho Eftimov <75941337+JordanchoEftimov@users.noreply.github.com>
2022-12-22 13:24:10 +0100

    Update api_public.php

--- routes/api_public.php
+++ routes/api_public.php
@@ -8,6 +8,6 @@ use App\Http\Controllers\ApiPublic\GiftCodeApplicantApiController;
 
 Route::get('/auth/check', 'Auth@check')->name('check');
 Route::get('/packages', 'PackageController@packages')->name('packages.index');
-Route::resource('firm-registrations', FirmRegistrationApiController::class)->only('index', 'store');
-Route::resource('firm-registration-attempts', FirmRegistrationAttemptApiController::class)->only('store');
+Route::apiResource('firm-registrations', FirmRegistrationApiController::class)->only('index', 'store');
+Route::apiResource('firm-registration-attempts', FirmRegistrationAttemptApiController::class)->only('store');
 Route::post('gift-code', [GiftCodeApplicantApiController::class, 'store'])->middleware('throttle:10,1');
